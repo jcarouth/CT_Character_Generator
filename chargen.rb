4f8bcd101d9becc809b343d2f7c96264ac57bf58
Leam Hall <leamhall@gmail.com>
2014-10-18 07:07:17 -0400

    Cleaned up chargen.rb to fit the new Character class.

--- chargen.rb
+++ chargen.rb
@@ -4,18 +4,19 @@
 require 'Character'
 require 'trav_functions.rb'
 
-me = Character.new('m', 'Scout', 'scuba')
+me = Character.new
 me.set_terms(average2)
 me.set_age
 my_age = me.get_age
 
-stats_names = ['Str', 'Dex', 'End', 'Int', 'Edu', 'Soc']
-stats_names.each do |stat|
+stat_names = ['Str', 'Dex', 'End', 'Int', 'Edu', 'Soc']
+stat_names.each do |stat|
   me.set_stat(stat, make_stat)
 end
 
 me.set_upp
 my_upp = me.get_upp
-puts "My age is #{my_age} and my UPP is #{my_upp}."
+me.set_career('Scout')
+puts "A #{my_age} year old #{me.get_career} with a UPP of #{my_upp}."
 
 
